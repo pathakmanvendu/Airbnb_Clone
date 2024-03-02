03e3c2a18126fc355afce2598f52d1279f514fc8
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-02 20:00:59 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Manvendu Pathak on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
