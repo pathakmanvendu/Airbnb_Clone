c227c3014c1f273e79dd15674823059c437bc557
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-04 19:30:43 +0530

    ApiService

--- Airbnb/Services/APIService.swift
+++ Airbnb/Services/APIService.swift
@@ -13,6 +13,31 @@ final class APIService {
     struct Constant {
         static let APIUrl = URL(string: "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/airbnb-listings/records?limit=100&refine=city%3A%22New%20York%22&refine=room_type%3A%22Entire%20home%2Fapt%22")
     }
+    
+    
+    public func getListings(completion: @escaping (Result<[AirbnbListing], Error>) -> Void){
+        guard let url = Constant.APIUrl else {
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data,error == nil else{
+                if let error {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(AirbnbListingResponse.self, from: data)
+                completion(.success(response.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
 }
  
 
